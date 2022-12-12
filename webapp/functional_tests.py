c447f9da70cbd810523b4cf991f726e3e0c64481
genndy007 <kochevgenazp@gmail.com>
2022-12-13 00:02:33 +0200

    Make test as class of unittest

--- webapp/functional_tests.py
+++ webapp/functional_tests.py
@@ -1,7 +1,37 @@
 from selenium import webdriver
+import unittest
 
-server_url = 'http://localhost:8000'
-browser = webdriver.Firefox()
-browser.get(server_url)
 
-assert 'success' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        self.url = "http://localhost:8000"
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_fill_list_and_retrieve_it_by_unique_url(self):
+        self.browser.get(self.url)
+        # User sees page title that has mention of to-do
+        self.assertIn("Todo", self.browser.title)
+        self.fail("Finish test")
+        # User is invited to enter a new to-do item
+
+        # User enters "Write text" into a text field
+
+        # User presses Enter, page updates with new item in list
+
+        # User is still invited to enter a new item
+
+        # User enters "Read text" into field, presses Enter, page updates with 2 items
+
+        # User needs webapp to remember list
+        # user sees generated unique URL for this list, with helping message
+
+        # User visits that generated URL, list is still there
+
+        # User is satisfied, closes browser
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
